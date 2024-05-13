ada84aa129499129ce8987aead30d9aa5aeaaf4a
iamfminsaf <iamfminsaf@gmail.com>
2024-05-13 19:11:12 +0530

    added frontend files

---
+++ client/src/App.jsx
@@ -0,0 +1,106 @@
+import axios from "axios";
+import { Movie } from "./components/Movie";
+import { useEffect, useState } from "react";
+
+const App = () => {
+    const [datas, setDatas] = useState([]);
+
+    const getDatas = async () => {
+        await axios.get("http://localhost:8000/data").then((res) => {
+            setDatas(res.data);
+        });
+    };
+
+    useEffect(() => {
+        getDatas();
+    }, []);
+
+    const deleteData = async (id) => {
+        await axios.delete(`http://localhost:8000/data/${id}`).then((res) => {
+            setDatas(res.data);
+        });
+    };
+
+    const tryAddData = () => {
+        const form = document.getElementById("form");
+        const app = document.getElementById("app");
+        app.style.opacity = "10%";
+        form.style.display = "flex";
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const title = e.target.elements.title.value;
+        const rate = e.target.elements.rate.value;
+        const genre = e.target.elements.genre.value.split(" ");
+        console.log(genre);
+        await axios
+            .post("http://localhost:8000/data", { title, rate, genre })
+            .then((res) => {
+                setDatas(res.data);
+            });
+        const form = document.getElementById("form");
+        const app = document.getElementById("app");
+        app.style.opacity = "100%";
+        form.style.display = "none";
+    };
+
+    return (
+        <>
+            <form
+                id="form"
+                onSubmit={(e) => {
+                    handleSubmit(e);
+                }}
+            >
+                <h2>Add new Movie</h2>
+                <input
+                    type="text"
+                    placeholder="Title"
+                    className="title-input"
+                    name="title"
+                />
+                <input
+                    type="number"
+                    className="rate-input"
+                    placeholder="Rating"
+                    name="rate"
+                />
+                <input
+                    type="text"
+                    className="genre-input"
+                    placeholder="Genres"
+                    name="genre"
+                />
+                <button type="submit">Add</button>
+            </form>
+            <div className="app" id="app">
+                <h1>Movies</h1>
+                <div className="header-input">
+                    <input
+                        type="search"
+                        name="search"
+                        className="search"
+                        placeholder="Search movies.."
+                    />
+                    <button className="btn add-btn" onClick={tryAddData}>
+                        Add New
+                    </button>
+                </div>
+                <div className="movies">
+                    {datas.map((data) => {
+                        return (
+                            <Movie
+                                key={data._id}
+                                data={data}
+                                fun={deleteData}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default App;
---
+++ client/src/components/Movie.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import posterTemp from "../img/temp.png";
+import axios from "axios";
+
+export const Movie = (probs) => {
+    const { title, poster, rate, genre, like, _id } = probs.data;
+    const deleteData = probs.fun;
+
+    const [movieLike, setMovieLike] = useState(like);
+
+    const putLike = async () => {
+        const newLike = await axios
+            .put(`http://localhost:8000/data/${_id}`)
+            .then((res) => {
+                return res.data.like;
+            });
+        setMovieLike(newLike);
+    };
+
+    return (
+        <div className="movie">
+            <img src={poster || posterTemp} alt="thump" />
+            <h2>{title}</h2>
+            <span>{rate}</span>
+            <ul>
+                {genre.map((genre, index) => {
+                    return <li key={index}>{genre}</li>;
+                })}
+            </ul>
+            <div className="buttons">
+                <button onClick={putLike} className="btn like-btn">
+                    {movieLike} Likes
+                </button>
+                <button
+                    className="btn delete-btn"
+                    onClick={() => deleteData(_id)}
+                >
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
+};
